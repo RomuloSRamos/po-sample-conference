71ddb5b1e93f1434cb6cf70a0dbbf8cb15f42ca5
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2020-11-11 18:31:14 -0300

    feat(api/web): inclui funcionalidade de envio de arquivos
    po-sample-api-conference - adiciona api's para criação, consulta, atualização
    e exclusão de photos da gallery.
    po-sample-app-conference - adiciona nova funcionalidade, uma galeria de photos
    para exemplificar o envio de arquivos com o po-sync.
    
    Fixes DTHFUI-4622

---
+++ po-sample-app-conference/src/app/gallery/gallery.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+import { PoSyncService } from '@po-ui/ng-sync';
+import { Subscription } from 'rxjs';
+
+import { GalleryService } from '../services/gallery.service';
+import { UserService } from '../services/user.service';
+import { Photos } from './gallery.interface';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss']
+})
+export class GalleryComponent {
+  photos: Photos = [];
+  onSyncSubscription: Subscription;
+  photosSubscription: Subscription;
+  isLogged = false;
+  untitledImage: 'Untitled image';
+
+  constructor(
+    private galleryService: GalleryService,
+    private poSync: PoSyncService,
+    private userService: UserService) {
+    this.userService.getLoggedUserId().then(user => this.isLogged = !!user);
+  }
+
+  ionViewWillEnter() {
+    this.loadPhotos();
+
+    this.onSyncSubscription = this.poSync.onSync().subscribe(() => this.loadPhotos());
+  }
+
+  ionViewWillLeave() {
+    this.onSyncSubscription?.unsubscribe();
+    this.photosSubscription?.unsubscribe();
+  }
+
+  async loadPhotos() {
+    this.photosSubscription = this.galleryService.list().subscribe(data => {
+      // tslint:disable-next-line:no-string-literal
+      if (this.photos !== data['items']) {
+        // tslint:disable-next-line:no-string-literal
+        this.photos = data['items'].filter(item => item.deleted === false);
+      }
+    });
+  }
+
+  setSRCImage(fileName: string) {
+    return `http://localhost:3000/files/${fileName}`;
+  }
+}
